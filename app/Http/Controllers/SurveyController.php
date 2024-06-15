f3b62175b2b32280affc9485da0ca017a8072d61
Nii Adjetey Sowah <petersowah@gmail.com>
2024-06-15 21:38:54 +0000

    Initial Commit

---
+++ app/Http/Controllers/SurveyController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Survey;
+use Illuminate\Http\Request;
+
+class SurveyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Survey $survey)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Survey $survey)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Survey $survey)
+    {
+        //
+    }
+}
